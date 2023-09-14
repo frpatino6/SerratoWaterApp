8c7a6d0f60501a62fb15bf4a06c561ef56da092c
Fernando Rodriguez <frpatino6@gmail.com>
2023-09-14 09:09:26 -0500

    blocs

---
+++ lib/bloc/sales_list/sales_state.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:serrato_water_app/models/sales_data.dart';
+
+@immutable
+abstract class SalesState {}
+
+class SalesInitial extends SalesState {}
+
+class SalesLoading extends SalesState {}
+
+class SalesLoaded extends SalesState {
+  final List<SaleData> salesData;
+
+  SalesLoaded(this.salesData);
+}
+
+class SalesError extends SalesState {
+  final String message;
+
+  SalesError(this.message);
+}
---
+++ lib/models/register.dart
@@ -0,0 +1,32 @@
+class User {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+
+  User({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'password':
+          password, // Consider storing a hashed version of the password instead
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      firstName: map['firstName'],
+      lastName: map['lastName'],
+      email: map['email'],
+      password: map['password'],
+    );
+  }
+}
